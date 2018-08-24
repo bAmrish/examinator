6e2fc973e4c10e8e2e5965a1414b6b08aeedde42
Amrish Bharatiya <amrish.bharatiya@nielsen.com>
2018-08-24 17:30:35 +0530

    removed classes folders from being tracked in the version control

--- classes/production/examinator-server/spring/resources.groovy
+++
@@ -1,5 +0,0 @@
-import com.examinator.security.authentication.UserPasswordEncoderListener
-// Place your Spring DSL code here
-beans = {
-    userPasswordEncoderListener(UserPasswordEncoderListener)
-}
